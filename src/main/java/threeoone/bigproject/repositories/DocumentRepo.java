fce1e3f45a60c490cec0ac362b2878fee6a06d80
ducbrick <ducbrick@gmail.com>
2024-10-21 12:59:23 +0700

    Add javadoc

--- src/main/java/threeoone/bigproject/repositories/DocumentRepo.java
+++ src/main/java/threeoone/bigproject/repositories/DocumentRepo.java
@@ -4,5 +4,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.ListCrudRepository;
 import threeoone.bigproject.entities.Document;
 
+/**
+ * Simple Spring Data JPA's repository for {@link Document} entity.
+ *
+ * @author DUCBRICK
+ */
 public interface DocumentRepo extends ListCrudRepository <Document, Integer> {
 }
