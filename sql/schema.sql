46d9e76c773b18e2eff642459edd099bbe15142a
ducbrick <ducbrick@gmail.com>
2024-10-22 07:59:48 +0700

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -4,4 +4,13 @@ CREATE TABLE Document (
 	document_id SMALLSERIAL PRIMARY KEY,
 	document_name VARCHAR(100) NOT NULL CHECK(LENGTH(TRIM(document_name)) > 0),
 	document_description BPCHAR
-)
+);
+
+DROP TABLE IF EXISTS AppUser;
+
+CREATE TABLE AppUser (
+	user_id SMALLSERIAL PRIMARY KEY,
+	user_name VARCHAR(32) NOT NULL CHECK(LENGTH(TRIM(user_name)) > 0),
+	user_password VARCHAR(32) NOT NULL CHECK(LENGTH(TRIM(user_password)) > 0)
+);
+
