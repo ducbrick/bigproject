e5c754196edbf13569aa84da33dad114092baeed
ducbrick <ducbrick@gmail.com>
2024-10-25 18:29:09 +0700

    Update schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,17 +1,17 @@
 DROP TABLE IF EXISTS Document;
-
-CREATE TABLE Document (
-	document_id SMALLSERIAL PRIMARY KEY,
-	document_name VARCHAR(100) NOT NULL CHECK(LENGTH(TRIM(document_name)) > 0),
-	document_description BPCHAR
-);
-
 DROP TABLE IF EXISTS AppUser;
 
 CREATE TABLE AppUser (
-	user_id SMALLSERIAL PRIMARY KEY,
+	id SMALLSERIAL PRIMARY KEY,
 	login_name VARCHAR(32) NOT NULL CHECK(LENGTH(TRIM(login_name)) > 0),
 	password VARCHAR(32) NOT NULL CHECK(LENGTH(TRIM(password)) > 0),
 	display_name VARCHAR(64) NOT NULL CHECK(LENGTH(TRIM(display_name)) > 0)
+);
+
+CREATE TABLE Document (
+	id SMALLSERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL CHECK(LENGTH(TRIM(name)) > 0),
+	description BPCHAR,
+	author_id SMALLINT REFERENCES AppUser NOT NULL
 );
 
