a518f9169754ebda9c3c03c68279250127445f7f
ducbrick <ducbrick@gmail.com>
2024-10-28 11:25:40 +0700

    Update Document javadoc

--- src/main/java/threeoone/bigproject/entities/Document.java
+++ src/main/java/threeoone/bigproject/entities/Document.java
@@ -12,9 +12,12 @@ import jakarta.persistence.Table;
 
 /**
  * ORM Entity representing a {@link Document} in the library database.
- * A {@link Document} has a primary key {@code id}, {@code name} and {@code description}.
+ * A {@link Document} has a primary key {@code id}, a {@code name} and a {@code description}.
+ * A {@link Document} has an {@code author},
+ * representing the {@link User} who published this {@link Document}.
  *
  * @see threeoone.bigproject.repositories.DocumentRepo
+ * @see User
  *
  * @author DUCBRICK
  */
@@ -36,42 +39,102 @@ public class Document {
   @JoinColumn(name = "author_id")
   private User author;
 
+  /**
+   * Constructs a new {@link Document} with the given {@code name} and {@code description}.
+   *
+   * @param name name of the new {@link Document}
+   * @param description description of the new {@link Document}
+   */
   public Document(String name, String description) {
     this.name = name;
     this.description = description;
   }
 
+  /**
+   * Constructs a new {@link Document}.
+   * This empty constructor is required by JPA.
+   */
   public Document() {
   }
 
+  /**
+   * Returns the {@code id} of the {@link Document}.
+   * This getter is required by JPA.
+   *
+   * @return the {@code id} of the {@link Document}
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Sets the {@code id} of the {@link Document}.
+   * This setter is required by JPA.
+   *
+   * @param id the new {@code id} of the {@link Document}
+   */
   public void setId(int id) {
     this.id = id;
   }
 
+  /**
+   * Returns the {@code name} of the {@link Document}.
+   * This getter is required by JPA.
+   *
+   * @return the {@code name} of the {@link Document}
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Sets the {@code name} of the {@link Document}.
+   * This setter is required by JPA.
+   *
+   * @param name the new {@code name} of the {@link Document}
+   */
   public void setName(String name) {
     this.name = name;
   }
 
+  /**
+   * Returns the {@code description} of the {@link Document}.
+   * This getter is required by JPA.
+   *
+   * @return the {@code description} of the {@link Document}
+   */
   public String getDescription() {
     return description;
   }
 
+  /**
+   * Sets the {@code description} of the {@link Document}.
+   * This setter is required by JPA.
+   *
+   * @param description the new {@code description} of the {@link Document}
+   */
   public void setDescription(String description) {
     this.description = description;
   }
 
+  /**
+   * Returns the {@code author} of the {@link Document},
+   * which is the {@link User} who published this {@link Document}.
+   * This getter is required by JPA.
+   *
+   * @return the {@code author} of the {@link Document}
+   */
   public User getAuthor() {
     return author;
   }
 
+  /**
+   * Sets the {@code author} of the {@link Document},
+   * which is the {@link User} who published this {@link Document}.
+   * This setter is required by JPA.
+   *
+   * @param author the new {@code author} of the {@link Document}
+   */
   public void setAuthor(User author) {
     this.author = author;
   }
