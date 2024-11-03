7eee15362d35240ad4ec76d3042501fc7058563c
ducbrick <ducbrick@gmail.com>
2024-11-03 17:48:48 +0700

    Update UserRegisterService.java

--- src/main/java/threeoone/bigproject/services/UserRegisterService.java
+++ src/main/java/threeoone/bigproject/services/UserRegisterService.java
@@ -17,16 +17,20 @@ import threeoone.bigproject.repositories.UserRepo;
 public class UserRegisterService {
   private final UserRepo userRepo;
   private final LoginService loginService;
+  private final IllegalCharacterFilterService illegalCharacterFilterService;
 
   /**
-   * Autowired constructor to obtain necessary dependencies.
+   * Autowired constructor to obtain necessary dependencies
    *
-   * @param userRepo a {@link User} repository
+   * @param userRepo a User repository
    * @param loginService a {@link LoginService}
+   * @param illegalCharacterFilterService User's loginName and password security check
    */
-  public UserRegisterService(UserRepo userRepo, LoginService loginService) {
+  public UserRegisterService(UserRepo userRepo, LoginService loginService,
+      IllegalCharacterFilterService illegalCharacterFilterService) {
     this.userRepo = userRepo;
     this.loginService = loginService;
+    this.illegalCharacterFilterService = illegalCharacterFilterService;
   }
 
   /**
@@ -36,8 +40,8 @@ public class UserRegisterService {
    *
    * @throws UserAlreadyExistException when another User with the same loginName exists
    * @throws IllegalCredentialsException when input User have illegal credentials, such as empty name etc
+   * @throws RuntimeException when fails to save User into the database
    */
-  //TODO: Check User constraints
   public void register(User user) throws UserAlreadyExistException, IllegalCredentialsException {
     if (user == null) {
       throw new IllegalArgumentException("Tried to register with a NULL User");
@@ -47,8 +51,19 @@ public class UserRegisterService {
       throw new AlreadyLoggedInException("Users shouldn't register while currently logged in");
     }
 
+    user.setLoginName(user.getLoginName().stripTrailing());
+    user.setDisplayName(user.getDisplayName().stripTrailing());
+
     if (userRepo.findByLoginName(user.getLoginName()) != null) {
       throw new UserAlreadyExistException("There is another registered user with that username");
+    }
+
+    if (illegalCharacterFilterService.hasIllegalCharacter(user.getLoginName())) {
+      throw new IllegalCredentialsException("Username can only contain alphabet characters and/or digits.");
+    }
+
+    if (illegalCharacterFilterService.hasIllegalCharacter(user.getPassword())) {
+      throw new IllegalCredentialsException("Password can only contain alphabet characters and/or digits.");
     }
 
     userRepo.save(user);
