c520520b9aa7d298fdb91e18e9eb27536f356c3a
purupurupkl <huybea123@gmail.com>
2024-11-03 15:02:14 +0700

    rebase

--- src/main/java/threeoone/bigproject/controller/viewcontrollers/MenuController.java
+++ src/main/java/threeoone/bigproject/controller/viewcontrollers/MenuController.java
@@ -1,6 +1,5 @@
 package threeoone.bigproject.controller.viewcontrollers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
@@ -12,50 +11,73 @@ import threeoone.bigproject.controller.RequestSender;
 import threeoone.bigproject.controller.SceneName;
 import threeoone.bigproject.controller.requestbodies.SwitchScene;
 
+/**
+ * @author purupurupkl
+ */
 @Component
 @FxmlView("Menu.fxml")
 public class MenuController implements ViewController {
-  private final RequestSender<SwitchScene> switchSceneRequestSender;
-  private final MenuBarController menuBarController;
-  /**
-   * Temporary page controller to show features.
-   * @param switchSceneRequestSender request sender for switch scene
-   */
-  public MenuController(RequestSender<SwitchScene> switchSceneRequestSender,
-                        MenuBarController menuBarController) {
-    this.switchSceneRequestSender = switchSceneRequestSender;
-    this.menuBarController = menuBarController;
-  }
+    private final RequestSender<SwitchScene> switchSceneRequestSender;
 
-  @FXML
-  private AnchorPane root;
+    @FXML
+    private AnchorPane root;
 
-  public void initialize() {
-    menuBarController.highlight(SceneName.MAIN_MENU);
-  }
-  /**
-   * @return the root {@link Parent} of this view
-   */
-  @Override
-  public Parent getParent() {
-    return root;
-  }
+    /**
+     * "document overview" for borrowing?
+     */
+    @FXML
+    private Button DocumentOverview;
 
-  /**
-   * Notifies the controller that its view is being displayed.
-   */
-  @Override
-  public void show() {
+    /**
+     *"Your Books" page
+     */
+    @FXML
+    private Button BorrowedBooks;
 
-  }
+    @FXML
+    private Button Featured;
 
-  /**
-   * Handles the event when the "Doc Overview" button is pressed.
-   *
-   * @param event the {@link ActionEvent} triggered by button press
-   */
-  @FXML
-  void pressDocOverview(ActionEvent event) {
-    switchSceneRequestSender.send(new SwitchScene(SceneName.DOC_OVERVIEW));
-  }
+    @FXML
+    private Button Random;
+
+    @Override
+    public Parent getParent() {
+        root.setPrefSize(1200, 800);
+        return root;
+    }
+
+    public MenuController(RequestSender<SwitchScene> switchSceneRequestSender) {
+        this.switchSceneRequestSender = switchSceneRequestSender;
+    }
+
+    @Override
+    public void show() {
+        Featured.setText("Featured book \n Nakano Miku character book \n Very nice little book about miku.");
+    }
+
+    private void displayUserInfo() {
+
+    }
+
+    private void borrowBooks() {
+
+    }
+
+    private void yourBooks() {
+        switchSceneRequestSender.send(new SwitchScene(SceneName.YOUR_BOOKS));
+    }
+
+    /**
+     * generate a random book
+     */
+    private void randomBook() {
+
+    }
+
+    /**
+     *
+     */
+    private void featuredBook() {
+
+    }
 }
