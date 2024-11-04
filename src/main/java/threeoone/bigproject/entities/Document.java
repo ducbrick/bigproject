23cc848da7392d2e57395da6d33dd878b1bb6eb1
ducbrick <ducbrick@gmail.com>
2024-11-04 11:27:26 +0700

    Upload Document PK

--- src/main/java/threeoone/bigproject/entities/Document.java
+++ src/main/java/threeoone/bigproject/entities/Document.java
@@ -28,7 +28,7 @@ public class Document {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id")
-  private int id;
+  private Integer id;
 
   @Column(name = "name")
   private String name;
