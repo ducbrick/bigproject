b8c7ccfe5b8ce59d3b97306298dd5f515278c754
ducbrick <ducbrick@gmail.com>
2024-11-09 21:40:11 +0700

    Add test

--- src/test/java/threeoone/bigproject/entities/DocumentTest.java
+++ src/test/java/threeoone/bigproject/entities/DocumentTest.java
@@ -8,20 +8,4 @@ import org.junit.jupiter.api.Test;
 import threeoone.bigproject.exceptions.IllegalDocumentInfoException;
 
 class DocumentTest {
-  @Test
-  @DisplayName("Check constraints for empty name")
-  public void constraintsEmptyName() {
-    User user = new User("username", "password");
-    Document document = new Document("  ", null);
-    user.addUploadedDocument(document);
-    assertThatThrownBy(document::checkConstraints).isInstanceOf(IllegalDocumentInfoException.class);
-  }
-
-  @Test
-  @DisplayName("Check constraints for NULL uploader")
-  public void constraintsNullUploader() {
-    User user = new User("username", "password");
-    Document document = new Document("name", null);
-    assertThatThrownBy(document::checkConstraints).isInstanceOf(IllegalDocumentInfoException.class);
-  }
 }
