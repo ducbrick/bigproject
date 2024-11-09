76c31c916ff816c444aecb64b2a25c0ea1ffd283
ducbrick <ducbrick@gmail.com>
2024-11-09 10:57:54 +0700

    Update MemberRepo.java

--- src/main/java/threeoone/bigproject/repositories/MemberRepo.java
+++ src/main/java/threeoone/bigproject/repositories/MemberRepo.java
@@ -16,11 +16,12 @@ public interface MemberRepo extends ListCrudRepository <Member, Integer> {
    * Retrieves a List of Members whose name contain the given substring.
    *
    * @apiNote the list returned will not be {@code NULL}.
+   * @apiNote this method is not case-sentitive.
    *
    * @param substr the substring to retrieve Members
    *
    * @return the List of Members whose name contain the given substring
    */
-  @Query("SELECT m from Member m WHERE m.name LIKE %:substr%")
+  @Query("SELECT m from Member m WHERE LOWER(m.name) LIKE LOWER(CONCAT('%', :substr, '%'))")
   List <Member> findWithNameContaining(@Param("substr") String substr);
 }
