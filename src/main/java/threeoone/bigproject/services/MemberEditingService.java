30e52d46bca281dc2405718fb12b5ee37f3b7ec4
ducbrick <ducbrick@gmail.com>
2024-11-09 16:53:34 +0700

    Implement service

---
+++ src/main/java/threeoone/bigproject/services/MemberEditingService.java
@@ -0,0 +1,54 @@
+package threeoone.bigproject.services;
+
+import java.util.NoSuchElementException;
+import org.springframework.stereotype.Service;
+import threeoone.bigproject.entities.Member;
+import threeoone.bigproject.repositories.MemberRepo;
+
+/**
+ * A service to modify, which is to add, update, or delete {@link Member} entities from the Database.
+ * This class is a singleton bean in Spring context.
+ *
+ * @author DUCBRICK 
+ */
+@Service
+public class MemberEditingService {
+  private final MemberRepo memberRepo;
+
+  /**
+   * Autowired constructors that obtain the necessary dependencies.
+   *
+   * @param memberRepo a repository for {@link Member}
+   */
+  public MemberEditingService(MemberRepo memberRepo) {
+    this.memberRepo = memberRepo;
+  }
+
+  /**
+   * Saves a new {@link Member} or update an existing {@link Member}.
+   * If the input {@link Member} has no {@code id} (or in other words, its {@code id} is {@code NULL}),
+   * this method saves a new {@link Member} to the Database.
+   * Otherwise, this method updates the pre-existing {@link Member} whose {@code id} is the same as the given Member's.
+   *
+   * @param member the {@link Member} to update
+   *
+   * @throws IllegalArgumentException when the given {@link Member} is {@code NULL}
+   * @throws NoSuchElementException when the given Member's {@code id} doesn't exist in the Database.
+   */
+  void update(Member member) {
+    if (member != null && member.getId() != null && !memberRepo.existsById(member.getId())) {
+      throw new NoSuchElementException("Attempting to update a non-existing Member");
+    }
+    memberRepo.save(member);
+  }
+
+  /**
+   * Deletes the {@link Member} with the given {@code id}.
+   * If the {@link Member} is not found in the persistence store it is silently ignored.
+   *
+   * @param id the {@code id} of the {@link Member} to delete
+   */
+  void delete(int id) {
+    memberRepo.deleteById(id);
+  }
+}
