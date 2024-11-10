11ec9a30e3e090c30f4e93eda6eb8544bfe33b67
ducbrick <ducbrick@gmail.com>
2024-11-10 22:08:13 +0700

    Update javadoc

--- src/main/java/threeoone/bigproject/repositories/MemberRepo.java
+++ src/main/java/threeoone/bigproject/repositories/MemberRepo.java
@@ -4,7 +4,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
+import threeoone.bigproject.entities.Document;
+import threeoone.bigproject.entities.LendingDetail;
 import threeoone.bigproject.entities.Member;
+import threeoone.bigproject.entities.User;
 
 /**
  * JPA repository for {@link Member} entity.
@@ -25,6 +28,19 @@ public interface MemberRepo extends ListCrudRepository <Member, Integer> {
   @Query("SELECT m from Member m WHERE LOWER(m.name) LIKE LOWER(CONCAT('%', :substr, '%'))")
   List <Member> findWithNameContaining(@Param("substr") String substr);
 
+  /**
+   * Retrieves a {@link Member} with a specific {@code id} and its {@code lendingDetails}.
+   * This method only executes a single SQL query.
+   * This method retrieves the desired {@link Member} and details all of its lending {@link Document},
+   * as opposed to {@link #findById}, which only retrieves the desired {@link Member} but not its {@code lendingDetails}.
+   *
+   * @param id the id of the desired {@link Member}
+   *
+   * @return the {@link Member} with the input id, along with its {@code lendingDetails}.
+   *
+   * @see Member
+   * @see LendingDetail
+   */
   @Query("SELECT m FROM Member m LEFT JOIN FETCH m.lendingDetails WHERE m.id = (:id)")
   Member findWithLendingDetails(@Param("id") int id);
 }
