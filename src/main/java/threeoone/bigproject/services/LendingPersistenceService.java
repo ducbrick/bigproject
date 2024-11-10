dde139afa7bf2a62e34bc94b9b8c8179eacf31f6
ducbrick <ducbrick@gmail.com>
2024-11-10 17:40:33 +0700

    Update LendingPersistenceService.java

--- src/main/java/threeoone/bigproject/services/LendingPersistenceService.java
+++ src/main/java/threeoone/bigproject/services/LendingPersistenceService.java
@@ -1,11 +1,14 @@
 package threeoone.bigproject.services;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
+import javax.print.Doc;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import threeoone.bigproject.entities.Document;
 import threeoone.bigproject.entities.LendingDetail;
+import threeoone.bigproject.entities.Member;
 import threeoone.bigproject.exceptions.IllegalDocumentInfoException;
 import threeoone.bigproject.repositories.LendingDetailRepo;
 
@@ -15,7 +18,27 @@ public class LendingPersistenceService {
   private final LendingDetailRepo lendingDetailRepo;
 
   /**
-   * Saves a new {@link LendingDetail}.
+   * Saves a new {@link LendingDetail} from the given {@link Member} and {@link Document}.
+   * This method constructs a new {@link LendingDetail} Entity instance
+   * from the given {@link Member}, {@link Document} and the {@link LocalDateTime} at the moment,
+   * sets up bidirectional relationships, and delegates to {@link #update}.
+   *
+   * @param member the {@link Member} that wants to lend
+   * @param document the {@link Document} that will be lent
+   *
+   * @throws RuntimeException when unexpected errors occur when working with Database (such as constraints violation)
+   *
+   * @return the constructed {@link LendingDetail}.
+   */
+  public LendingDetail lend(@NonNull Member member, @NonNull Document document) {
+    LendingDetail lendingDetail = new LendingDetail(LocalDateTime.now());
+    member.lendDocument(lendingDetail);
+    document.lendDocument(lendingDetail);
+    return update(lendingDetail);
+  }
+
+  /**
+   * Saves a {@link LendingDetail}.
    * <p>
    * If the given {@link LendingDetail} has no {@code id} (or in other words, its {@code id} is {@code NULL}),
    * this method adds a new {@link LendingDetail} to the Database.
