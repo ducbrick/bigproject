848f0c4bd535e39bd8fa649643477450d9094f1c
ducbrick <ducbrick@gmail.com>
2024-11-11 16:15:30 +0700

    Update UserRegisterService.java

--- src/main/java/threeoone/bigproject/services/UserRegisterService.java
+++ src/main/java/threeoone/bigproject/services/UserRegisterService.java
@@ -1,6 +1,9 @@
 package threeoone.bigproject.services;
 
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import threeoone.bigproject.entities.User;
 import threeoone.bigproject.exceptions.AlreadyLoggedInException;
 import threeoone.bigproject.exceptions.IllegalCredentialsException;
@@ -14,24 +17,11 @@ import threeoone.bigproject.repositories.UserRepo;
  * @author DUCBRICK
  */
 @Service
+@RequiredArgsConstructor
 public class UserRegisterService {
   private final UserRepo userRepo;
   private final LoginService loginService;
   private final IllegalCharacterFilterService illegalCharacterFilterService;
-
-  /**
-   * Autowired constructor to obtain necessary dependencies
-   *
-   * @param userRepo a User repository
-   * @param loginService a {@link LoginService}
-   * @param illegalCharacterFilterService User's loginName and password security check
-   */
-  public UserRegisterService(UserRepo userRepo, LoginService loginService,
-      IllegalCharacterFilterService illegalCharacterFilterService) {
-    this.userRepo = userRepo;
-    this.loginService = loginService;
-    this.illegalCharacterFilterService = illegalCharacterFilterService;
-  }
 
   /**
    * Register a new {@link User} into the Database.
@@ -43,11 +33,8 @@ public class UserRegisterService {
    * @throws IllegalCredentialsException when input User have illegal credentials, such as empty name etc
    * @throws RuntimeException when fails to save User into the database
    */
-  public void register(User user) throws UserAlreadyExistException, IllegalCredentialsException {
-    if (user == null) {
-      throw new IllegalArgumentException("Tried to register with a NULL User");
-    }
-
+  @Transactional
+  public void register(@NonNull User user) throws UserAlreadyExistException, IllegalCredentialsException {
     if (loginService.getLoggedInUserId() != null) {
       throw new AlreadyLoggedInException("Users shouldn't register while currently logged in");
     }
@@ -66,15 +53,7 @@ public class UserRegisterService {
       throw new UserAlreadyExistException("There is another registered user with that username");
     }
 
-    if (illegalCharacterFilterService.hasIllegalCharacter(user.getUsername())) {
-      throw new IllegalCredentialsException("Username can only contain alphabet characters and/or digits.");
-    }
-
-    if (illegalCharacterFilterService.hasIllegalCharacter(user.getPassword())) {
-      throw new IllegalCredentialsException("Password can only contain alphabet characters and/or digits.");
-    }
-
+    user.setId(null);
     userRepo.save(user);
-    loginService.login(user);
   }
 }
