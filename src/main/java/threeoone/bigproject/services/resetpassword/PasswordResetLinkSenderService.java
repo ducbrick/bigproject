9aa437b03f0203e0375c15c305a37b68f62a05a1
ducbrick <ducbrick@gmail.com>
2024-11-28 22:44:32 +0700

    Update javadoc

--- src/main/java/threeoone/bigproject/services/resetpassword/PasswordResetLinkSenderService.java
+++ src/main/java/threeoone/bigproject/services/resetpassword/PasswordResetLinkSenderService.java
@@ -17,10 +17,19 @@ import threeoone.bigproject.repositories.UserRepo;
 import threeoone.bigproject.services.EmailSenderService;
 import threeoone.bigproject.services.RandomStringGeneratorService;
 
+/**
+ * A service to send emails to Users containing the REST API uri that will authenticate requests to reset their password.
+ * This class is a singleton bean in Spring context.
+ *
+ * @author DUCBRICK
+ */
 @Service
 @Validated
 @RequiredArgsConstructor
 public class PasswordResetLinkSenderService {
+  /**
+   * Default valid duration of generated {@link PasswordResetToken}.
+   */
   public static int VALID_TIME_IN_MINUTES = 10;
 
   private final EmailSenderService emailSender;
@@ -39,6 +48,17 @@ public class PasswordResetLinkSenderService {
 
   private final Logger logger = LoggerFactory.getLogger(PasswordResetLinkSenderService.class);
 
+  /**
+   * Sends a password-reset email to the email address of a {@link User} with a specific {@code username}.
+   * <p>
+   * This method deletes any old {@link PasswordResetToken} associated to the given {@link User} and generate a new one.
+   * The new generated {@link PasswordResetToken} will be valid for the duration of {@link #VALID_TIME_IN_MINUTES}.
+   * During that time, it can be used to authenticate a single password-reset request of the specified {@link User}.
+   *
+   * @param username the username of the {@link User} who wants to reset their password
+   *
+   * @return whether if a {@link User} with the provided {@code username} exists
+   */
   @Transactional
   public boolean sendResetLink(String username) {
     User user = userRepo.findByUsername(username);
