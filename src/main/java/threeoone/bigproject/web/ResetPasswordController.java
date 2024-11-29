93a654b402700418511e52967bff17b92cf729fc
ducbrick <ducbrick@gmail.com>
2024-11-29 13:31:35 +0700

    Add dependency

---
+++ src/main/java/threeoone/bigproject/web/ResetPasswordController.java
@@ -0,0 +1,13 @@
+package threeoone.bigproject.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ResetPasswordController {
+  @GetMapping("${api.resetpassword.endpoint}")
+  public String resetPassword(String tokenValue) {
+    return "HELLO";
+  }
+}
