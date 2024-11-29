744578837f68af9c6d4955dee286a0297ab5f99e
ducbrick <ducbrick@gmail.com>
2024-11-29 21:50:26 +0700

    Update javadoc

--- src/main/java/threeoone/bigproject/services/resetpassword/PasswordResetAuthenticationService.java
+++ src/main/java/threeoone/bigproject/services/resetpassword/PasswordResetAuthenticationService.java
@@ -8,11 +8,27 @@ import threeoone.bigproject.entities.PasswordResetToken;
 import threeoone.bigproject.entities.User;
 import threeoone.bigproject.repositories.PasswordResetTokenRepo;
 
+/**
+ * A service to authenticate requests to reset a User's password.
+ * This class is a singleton bean in Spring context.
+ *
+ * @author DUCBRICK
+ */
 @Service
 @RequiredArgsConstructor
 public class PasswordResetAuthenticationService {
   private final PasswordResetTokenRepo tokenRepo;
 
+  /**
+   * Authenticate a request using a given token.
+   * <p>
+   * This method retrieves a single unique valid {@link PasswordResetToken} with a specific value
+   * and returns its associated {@link User}.
+   *
+   * @param tokenValue the value of the {@link PasswordResetToken}
+   *
+   * @return the {@link User} associated to the given token, of {@code NULL} if there isn't any
+   */
   @Transactional
   public User authenticate(String tokenValue) {
     var token = tokenRepo.findByValue(tokenValue);
