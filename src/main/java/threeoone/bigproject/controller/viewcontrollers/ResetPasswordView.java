6bf0b688643c90a242a37a4906aa9a6198ae8be2
ducbrick <ducbrick@gmail.com>
2024-12-01 18:52:36 +0700

    Add button to login

--- src/main/java/threeoone/bigproject/controller/viewcontrollers/ResetPasswordView.java
+++ src/main/java/threeoone/bigproject/controller/viewcontrollers/ResetPasswordView.java
@@ -5,14 +5,16 @@ import javafx.scene.Parent;
 import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
+import threeoone.bigproject.controller.RequestSender;
 
 @Component
 @RequiredArgsConstructor
 @FxmlView("resetpassword.fxml")
 public class ResetPasswordView implements ViewController {
+  private final RequestSender <ViewController> switchToLogin;
+
   @FXML
   private Parent root;
-
 
   @Override
   public Parent getParent() {
@@ -21,5 +23,10 @@ public class ResetPasswordView implements ViewController {
 
   @Override
   public void show() {
+  }
+
+  @FXML
+  private void switchToLogin() {
+    switchToLogin.send(null);
   }
 }
