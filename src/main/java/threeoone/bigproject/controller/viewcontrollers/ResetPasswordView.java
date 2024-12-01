026b4963cb7ccb4abf82c04041143780b5dfddaf
ducbrick <ducbrick@gmail.com>
2024-12-01 19:10:50 +0700

    add matching password constraint

--- src/main/java/threeoone/bigproject/controller/viewcontrollers/ResetPasswordView.java
+++ src/main/java/threeoone/bigproject/controller/viewcontrollers/ResetPasswordView.java
@@ -1,7 +1,10 @@
 package threeoone.bigproject.controller.viewcontrollers;
 
+import java.util.Objects;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
@@ -16,17 +19,51 @@ public class ResetPasswordView implements ViewController {
   @FXML
   private Parent root;
 
+  @FXML
+  private TextField passwordField;
+
+  @FXML
+  private TextField reEnteredPasswordField;
+
+  @FXML
+  private Label errorMessage;
+
   @Override
   public Parent getParent() {
     return root;
   }
 
+  public void showErrorMessage() {
+    errorMessage.setVisible(true);
+    errorMessage.setManaged(true);
+  }
+
+  public void hideErrorMessage() {
+    errorMessage.setVisible(false);
+    errorMessage.setManaged(false);
+  }
+
   @Override
   public void show() {
+    hideErrorMessage();
   }
 
   @FXML
   private void switchToLogin() {
     switchToLogin.send(null);
+  }
+
+  private boolean matchReEnteredPassword() {
+    String password = passwordField.getText();
+    String reEnteredPassword = reEnteredPasswordField.getText();
+    return (Objects.equals(password, reEnteredPassword));
+  }
+
+  @FXML
+  private void submitRequest() {
+    if (!matchReEnteredPassword()) {
+      showErrorMessage();
+      return;
+    }
   }
 }
