a6f87e514fa8a57287524c9713d5a50cda338697
Huy1902 <88786802+Huy1902@users.noreply.github.com>
2024-12-03 21:26:42 +0700

    Fix test

--- src/main/java/threeoone/bigproject/services/GoogleAPIService.java
+++ src/main/java/threeoone/bigproject/services/GoogleAPIService.java
@@ -3,6 +3,7 @@ package threeoone.bigproject.services;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import javafx.application.Platform;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
@@ -25,12 +26,14 @@ import threeoone.bigproject.util.Alerts;
 public class GoogleAPIService {
 
   @Value("${google.api.url}")
+  @Setter
   private String googleBooksURL;
 
   @Value("${google.api.timeout}")
   private int googleBooksTimeout;
 
   @Value("${google.api.key}")
+  @Setter
   private String googleBooksKey;
 
   /**
@@ -41,34 +44,33 @@ public class GoogleAPIService {
    */
   public Document findBookByISBN(String isbn) {
     JsonObject response = getJson(googleBooksURL + "isbn:" + isbn + "&key=" + googleBooksKey);
-    if(response == null) {
+    if (response == null) {
       return null;
     }
-    if(response.get("totalItems").getAsInt() == 0) {
+    if (response.get("totalItems").getAsInt() == 0) {
       Alerts.showAlertWarning("Error!!!", "Not found");
       return null;
     }
     JsonObject info = response.get("items").getAsJsonArray().get(0).getAsJsonObject().get("volumeInfo").getAsJsonObject();
     Document document = new Document(info.get("title").getAsString(), info.get("description").getAsString());
     StringBuilder authors = new StringBuilder();
-    if(info.get("authors") != null) {
-      for(JsonElement author : info.get("authors").getAsJsonArray()) {
+    if (info.get("authors") != null) {
+      for (JsonElement author : info.get("authors").getAsJsonArray()) {
         authors.append(author.getAsString()).append(", ");
       }
       authors.delete(authors.length() - 2, authors.length());
       document.setAuthor(authors.toString());
     }
     document.setIsbn(isbn);
-    if(info.get("categories") != null){
+    if (info.get("categories") != null) {
       StringBuilder categories = new StringBuilder();
-      for(JsonElement category : info.get("categories").getAsJsonArray()) {
+      for (JsonElement category : info.get("categories").getAsJsonArray()) {
         categories.append(category.getAsString()).append(", ");
       }
       categories.delete(categories.length() - 2, categories.length());
       document.setCategory(categories.toString());
-    }
-    else document.setCategory("Unknown");
-    if(info.get("imageLinks") != null) {
+    } else document.setCategory("Unknown");
+    if (info.get("imageLinks") != null) {
       document.setCoverImageUrl(info.get("imageLinks").getAsJsonObject().get("thumbnail").getAsString());
     }
     return document;
@@ -105,12 +107,13 @@ public class GoogleAPIService {
     try {
       response = getRestTemplate().getForEntity(url, String.class);
     } catch (RestClientException e) {
-      Alerts.showAlertWarning("Error!!!", e.getMessage());
+      Platform.runLater(() -> Alerts.showAlertWarning("Error!!!", e.getMessage()));
       return null;
     }
 
     if (!response.getStatusCode().is2xxSuccessful()) {
-      Alerts.showAlertWarning("Error!!!" + response.getStatusCode(), response.getBody());
+      ResponseEntity<String> finalResponse = response;
+      Platform.runLater(() -> Alerts.showAlertWarning("Error!!!" + finalResponse.getStatusCode(), finalResponse.getBody()));
       return null;
     }
 
